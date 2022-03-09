40a3468c1ee1d734dfba6172c868d7bb59c5a723
lsteuernagel <lsteuernagel@blade10.sf.mpg.de>
2022-03-09 17:37:47 +0100

    added gene reomval to hvg in seurat_recipe

--- R/processing_functions.R
+++ R/processing_functions.R
@@ -75,11 +75,14 @@ ReadDGEFormat= function(dge, feature.column = 1){
 #' @param seurat_object seurat object
 #' @param assay which assay
 #' @param nfeatures_vst number of features for HVG detection
-#' @param clean_hvg clean up HVGs with basic regex
+#' @param sample_column a column in meatdata with sample or batch ids. defaults to NULL which means feature detection will not be batch aware
+#' @param remove_hvgs  boolean. TRUE: use genes_to_remove to remove from HVGS
+#' @param genes_to_remove a vector with genes to exclude
 #' @param normalize_data normalize data with standard logNorm
 #' @param npcs_PCA how many pcs to save
 #' @param findClusters run neighbor and cluster detection with seurat standard
 #' @param calcUMAP calc standard seurat UMAP
+#' @param k.param k neighbors for UMAP and SNN
 #' @param clusterRes resolution for clustering
 #' @param key name/key for dimred (will name PCA like this and then use for all following steps)
 #' @param seed random seed
@@ -90,38 +93,43 @@ ReadDGEFormat= function(dge, feature.column = 1){
 #'
 #' @import Seurat
 
-# run seurat standard pipeline on an onject
-seurat_recipe = function(seurat_object,assay="RNA",nfeatures_vst = 2000,clean_hvg=FALSE,normalize_data=TRUE,npcs_PCA = 50,findClusters =F,calcUMAP=TRUE,clusterRes = 1,key="pca",seed=123){
+seurat_recipe = function(seurat_object,assay="RNA",nfeatures_vst = 1000,sample_column = NULL,remove_hvgs = FALSE,genes_to_remove = character(0),normalize_data=TRUE,npcs_PCA = 50,calcUMAP=TRUE,k.param=20,findClusters =F,clusterRes = 1,key="pca",seed=123){
   message("Running standard seurat recipe.")
   set.seed(seed)
   if(dim(seurat_object@assays[[assay]]@data)[2]==0){normalize_data=TRUE}
   if(normalize_data){
     seurat_object <- Seurat::NormalizeData(object = seurat_object, verbose = F,assay=assay)
   }else{
-    message("Skipping Normalization")
+    message("Warning: Skipping Normalization")
   }
-  message("Find HVGs")
-  seurat_object <- Seurat::FindVariableFeatures(object = seurat_object,assay=assay, selection.method = "vst", nfeatures = nfeatures_vst, verbose = F)
-  # ignore mt and rpl/s genes in hvg
-  if(clean_hvg){
-    idx = grep("mt-|Rpl|Rps",seurat_object@assays[[assay]]@var.features)
-    if(length(idx)>0){
-      print(paste0("Ignoring ",length(idx)," rp and mt genes in hvgs.."))
-      seurat_object@assays[[assay]]@var.features = seurat_object@assays[[assay]]@var.features[-idx]
-      seurat_object@assays[[assay]]@meta.features$vst.variable[idx] = FALSE
-    }
+
+  if(!is.null(sample_column)){
+    message("Find HVGs split by ",sample_column)
+    var_features = identify_variable_features(seurat_object,n_hvgs_sizes=nfeatures_vst*2,batch_var=sample_column,assay_name=assay,method="vst",ignore_genes_regex=NULL,returnSeurat=TRUE,seed=seed)
+  }else{
+    message("Find HVGs")
+    seurat_object <- Seurat::FindVariableFeatures(object = seurat_object,assay=assay, selection.method = "vst", nfeatures = nfeatures_vst*2, verbose = F)
+    # var features
+    var_features = seurat_object@assays[[assay]]@var.features
   }
+  # remove features:
+  if(remove_hvgs){
+    var_features = var_features[!var_features %in% genes_to_remove] # remove problematic genes
+  }
+  var_features = var_features[1:nfeatures_vst] # subset to actual length
+
+  # optional downstream:
   message("Run Scale and PCA")
-  seurat_object <- Seurat::ScaleData(object = seurat_object,assay=assay, verbose = F)
+  seurat_object <- Seurat::ScaleData(object = seurat_object,assay=assay,features = var_features, verbose = F)
   seurat_object <- Seurat::RunPCA(object = seurat_object,assay=assay, npcs = npcs_PCA,reduction.name = key,reduction.key=key, verbose = F,seed.use = seed)
-  message("Run Umap...")
-  if(key=="pca"){umap_key="umap"}else{umap_key=paste0("umap_",key)}
   if(calcUMAP){
-    seurat_object <- Seurat::RunUMAP(object = seurat_object,assay=assay, reduction = key,reduction.name=umap_key,reduction.key=umap_key, dims = 1:npcs_PCA,verbose = F,seed.use = seed)
+    message("Run Umap...")
+    if(key=="pca"){umap_key="umap"}else{umap_key=paste0("umap_",key)}
+    seurat_object <- Seurat::RunUMAP(object = seurat_object,assay=assay, reduction = key,reduction.name=umap_key,reduction.key=umap_key, dims = 1:npcs_PCA,n.neighbors=k.param,verbose = F,seed.use = seed)
   }
   if(findClusters){
     message("Run Graph & Clustering...")
-    seurat_object <- Seurat::FindNeighbors(seurat_object,reduction = key, dims = 1:npcs_PCA,k.param = 20,verbose = F)
+    seurat_object <- Seurat::FindNeighbors(seurat_object,reduction = key, dims = 1:npcs_PCA,k.param = k.param,verbose = F)
     seurat_object <- Seurat::FindClusters(seurat_object, resolution = clusterRes,verbose = F,random.seed = seed)
 
   }
@@ -137,10 +145,11 @@ seurat_recipe = function(seurat_object,assay="RNA",nfeatures_vst = 2000,clean_hv
 #'
 #' @param seurat_object seurat object
 #' @param n_hvgs_sizes integer or integer vector of desired feature set sizes
-#' @param batch_var where to store the results
+#' @param batch_var var to split by
 #' @param assay_name seurat object assay
 #' @param method which HVG method : selection.method from Seurat::FindVariableFeatures
 #' @param ignore_genes_regex  a regex o filter genes from HVGs
+#' @param returnSeurat return seurat
 #' @param seed random seed
 #'
 #' @return seurat_object with variable features in misc$var_features
@@ -149,7 +158,7 @@ seurat_recipe = function(seurat_object,assay="RNA",nfeatures_vst = 2000,clean_hv
 #'
 #' @import Seurat
 
-identify_variable_features = function(seurat_object,n_hvgs_sizes=1000,batch_var,assay_name="RNA",method="vst",ignore_genes_regex=NULL,seed=123){
+identify_variable_features = function(seurat_object,n_hvgs_sizes=1000,batch_var,assay_name="RNA",method="vst",ignore_genes_regex=NULL,returnSeurat=TRUE,seed=123){
   require(data.table)
   require(Seurat)
   # find features for largest set:
@@ -172,7 +181,12 @@ identify_variable_features = function(seurat_object,n_hvgs_sizes=1000,batch_var,
       seurat_object@misc$var_features[[paste0(assay_name,".log.",method,".split_",batch_var,".features.",hvg_genes)]] = all_features[1:hvg_genes]
     }
   }
-  return(seurat_object)
+
+  if(returnSeurat){
+    return(seurat_object)
+  }else{
+    return(seurat_object@misc$var_features[[paste0(assay_name,".log.",method,".split_",batch_var,".features.",hvg_genes)]])
+  }
 }
 
 ##########
